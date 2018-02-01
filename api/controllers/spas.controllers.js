3e6e9e5d16dcafeb4f0739472d64ae8ef247d31f
asimov <i@briangroat.com>
2018-02-01 11:24:15 -0500

    - Created a Database with MongoDB - Integrated this Database with the controllers - Imported the JSON Data to MongoDB

--- api/controllers/spas.controllers.js
+++ api/controllers/spas.controllers.js
@@ -1,13 +1,19 @@
+var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectID;
+
 var spaData = require('../data/spa-data.json');
 
 module.exports.spasGetAll = function(req, res) {
 
+  var db = dbconn.get();
+
   console.log('GET the spas');
   console.log(req.query);
 
-  var returnData;
   var offset = 0;
   var count = 5;
+
+  var collection = db.collection('spas');
 
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
@@ -17,24 +23,56 @@ module.exports.spasGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
-  returnData = spaData.slice(offset,offset+count);
+  collection
+    .find()
+    .skip(offset)
+    .limit(count)
+    .toArray(function(err, docs) {
+      console.log("Found spas", docs.length);
+      res
+        .status(200)
+        .json(docs);
+  });
 
-  res
-    .status(200)
-    .json(returnData);
 };
 
 module.exports.spasGetOne = function(req, res) {
-  console.log('GET spaId', req.params.spaId);
-  res
-    .status(200)
-    .json(spaData[req.params.spaId]);
+  var db = dbconn.get();
+  var id = req.params.spaId;
+  var collection = db.collection('spas');
+  console.log('GET spaId', id);
+
+  collection
+    .findOne({
+      _id : ObjectId(id)
+    }, function(err, doc) {
+      res
+        .status(200)
+        .json(doc);
+  });
+
 };
 
 module.exports.spasAddOne = function(req, res) {
   console.log("POST new spa");
-  console.log(req.body);
-  res
-    .status(200)
-    .json(req.body);
+  var db = dbconn.get();
+  var collection = db.collection('spas');
+  var newSpa;
+
+  if (req.body && req.body.name && req.body.stars) {
+    newSpa = req.body;
+    newSpa.stars = parseInt(req.body.stars, 10);
+    collection.insertOne(newSpa, function(err, response) {
+      console.log("Spa added", response, response.ops);
+      res
+        .status(201)
+        .json(response.ops);
+    });
+  } else {
+    console.log("Data missing from body");
+    res
+      .status(400)
+      .json({ message : "Required data missing from body" });
+  }
+
 };
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # MEAN Stack Demo
 
 This repo is an example of a MEAN stack application with user authentication. A listing of venues, and the ability to review those venues.
+
+To import JSON data into MongoDB run command "mongoimport -­‐db spasRUs -­‐collection	spas -­‐jsonArray api/data/spa-data.json"
